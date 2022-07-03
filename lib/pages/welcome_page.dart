9a86c14311d0734853aa2d85d0cad288bdb5ed9f
FlutLab.io <>
2022-07-03 12:43:54 +0000

    Autosave

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -19,8 +19,8 @@ class _WelcomePageState extends State<WelcomePage> {
   ];
 
   //var blur image
-  double _sigmaX = 0; // from 0-10
-  double _sigmaY = 0; // from 0-10
+  double _sigmaX = 0.3; // from 0-10
+  double _sigmaY = 0.3; // from 0-10
   double _opacity = 0; // from 0-1.0
 
   @override
