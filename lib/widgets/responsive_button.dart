8999ec123e5859e6c2da376e9cf3a29f57da8c58
FlutLab.io <>
2022-07-08 08:28:52 +0000

    Autosave

--- lib/widgets/responsive_button.dart
+++ lib/widgets/responsive_button.dart
@@ -6,7 +6,6 @@ class ResponsiveButton extends StatelessWidget {
   // variabel
   double? lebar;
   bool? isResponsive;
-  final String dogUrl = 'https://www.svgrepo.com/show/2046/dog.svg';
 
   // class untuk memanggil
   ResponsiveButton({Key? key, this.lebar, this.isResponsive = false}) : super(key: key);
@@ -14,11 +13,11 @@ class ResponsiveButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: lebar,
-      child: SvgPicture.string('''<svg viewBox="0 0 200 200"
-  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <image xlink:href="https://mdn.mozillademos.org/files/6457/mdn_logo_only_color.png" height="200" width="200"/>
-</svg>'''),
-    );
+        width: lebar,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.green[900],
+        ));
   }
 }
