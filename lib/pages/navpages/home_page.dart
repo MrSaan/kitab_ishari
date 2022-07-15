39386fa1006a2495d8c21fedba6b63009822e881
FlutLab.io <>
2022-07-15 01:09:05 +0000

    Autosave

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -12,6 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
