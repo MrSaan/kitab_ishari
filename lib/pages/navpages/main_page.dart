a1e9748eebb540d1fd20c83f08cd5e286d5bebd1
FlutLab.io <>
2022-07-15 00:20:10 +0000

    Autosave

--- lib/pages/navpages/main_page.dart
+++ lib/pages/navpages/main_page.dart
@@ -11,8 +11,11 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
         selectedItemColor: Colors.black54,
-        unselectedItemColor: Colors.grey,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        elevation: 0,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.apps), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.book), label: "Book"),
