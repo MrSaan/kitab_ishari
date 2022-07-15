02f2bac2e3a68ae959eec9ed4f1615294174a4a2
FlutLab.io <>
2022-07-15 00:07:11 +0000

    Autosave

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.apps), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.book))
+        ],
+      ),
+    );
+  }
+}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.apps), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: "Book"),
+          BottomNavigationBarItem(icon: Icon(Icons.event), label: "Event"),
+          BottomNavigationBarItem(icon: Icon(Icons.bookmark_border), label: "Bookmark"),
+        ],
+      ),
+    );
+  }
+}
