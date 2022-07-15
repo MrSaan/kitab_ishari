e215161c1df876195876d70b31024d1a8b83934e
FlutLab.io <>
2022-07-15 01:00:14 +0000

    Autosave

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -15,11 +15,20 @@ class _HomePageState extends State<HomePage> {
           Container(
             padding: const EdgeInsets.only(top: 70, left: 20),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Icon(
                   Icons.menu,
                   size: 30,
                   color: Colors.black54,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
                 )
               ],
             ),
