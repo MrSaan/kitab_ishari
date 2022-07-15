116e22ada44dd4647cd0f8f47492c77f216c0ada
FlutLab.io <>
2022-07-15 00:55:40 +0000

    Autosave

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,9 +9,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("My Home Page"),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: Colors.black54,
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
